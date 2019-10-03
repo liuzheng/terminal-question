3a09e98d569d969cbc38230b437d4a9802b82a44
liuzheng <liuzheng712@gmail.com>
2019-10-04 06:38:00 +0800

    feat: update

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/liuzheng/terminal-question
 
 go 1.12
-
-require golang.org/x/crypto v0.0.0-20191001103751-88343688bb37
--- structs.go
+++ structs.go
@@ -1,10 +1,8 @@
 package terminal_question
 
-import "golang.org/x/crypto/ssh/terminal"
-
 type Question struct {
   ID       string
   Question string
   Retry    int
-  Checker  func(string, *terminal.Terminal) bool
+  Checker  func(string) (bool, error)
 }
